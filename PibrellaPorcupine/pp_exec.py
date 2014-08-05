ac4068bb0ddd9b64e8e3befbefa970ca77da1456
rifoolaw <riflw@thehowl.it>
2014-08-05 21:16:02 +0100

    PibrellaPorcupine: FadingFast and Flash

--- PibrellaPorcupine/pp_exec.py
+++ PibrellaPorcupine/pp_exec.py
@@ -5,10 +5,13 @@ import pibrellaporcupine
 print "done. Welcome to Pibrella Porcupine.\n\n"
 while True:
     print "Welcome to Pibrella Porcupine! The sequences and \"Games\" that we have is the following:"
-    print "Sequences: HelloPorcupine "
+    print "Sequences: HelloPorcupine FadingFast Flash"
     print "Games:     "
     print "\n\nTo start one of them, just type its name and digit enter!\nYou can quit Pibrella Porcupine anytime by pressing CTRL + C"
     what = raw_input("")
     if what == "HelloPorcupine":
         pibrellaporcupine.HelloPorcupine()
-    
+    elif what == "FadingFast":
+        pibrellaporcupine.FadingFast()
+    elif what == "Flash":
+        pibrellaporcupine.Flash()
